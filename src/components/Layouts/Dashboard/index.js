4cdd80f9dcd086daec4e6897d74eecac9b52a070
Jonathan Uribe <99708920+JonathanPvc@users.noreply.github.com>
2022-07-18 18:07:04 -0500

    Jonathan (#5)
    * bajar rama
    
    * Lunes act
    
    * sincronizamos

--- src/components/Layouts/Dashboard/index.js
+++ src/components/Layouts/Dashboard/index.js
@@ -3,19 +3,19 @@ import NavBar from '../../NavBar/NavBar';
 import Sidebar from '../../Sidebar';
 
 
-export const Dashboard = ({children}) => {
+export const Dashboard = ({ children }) => {
   return (
     <>
-      <div className='flex h-full bg-orange-400 rounded-3xl '>
-        <Sidebar />
-        <div className='content w-full'>
-          <div className='bg-orange-200  rounded-3xl m-4 '>
-         {children}
+        <div className='flex w-full bg-orange-400 h-screen'>
+          <Sidebar />
+          <div className='w-full p-5'>
+            <div className='bg-orange-200 h-full rounded-3xl  h-full p-5'>
+              {children}
+            </div>
+
           </div>
 
         </div>
-
-      </div>
 
     </>
   )

de46a8862dc435066f86249fafc7e2472faa96d7
[Jonathan] <jonathan13pvc@gmail.com>
2022-07-18 17:06:36 -0500

    Lunes act

--- src/components/Layouts/Dashboard/index.js
+++ src/components/Layouts/Dashboard/index.js
@@ -3,18 +3,20 @@ import NavBar from '../../NavBar/NavBar';
 import Sidebar from '../../Sidebar';
 
 
-export const Dashboard = ({children}) => {
+export const Dashboard = ({ children }) => {
   return (
     <>
-      <div className='flex h-full bg-orange-400 rounded-3xl '>
-        <Sidebar />
-        <div className='content w-full'>
-          <div className='bg-orange-200  rounded-3xl m-4 h-full'>
-         {children}
+      <div className=' flex  h-full bg-slate-500 '>
+        <div className='flex w-full bg-orange-400 rounded-3xl m-4 '>
+          <Sidebar />
+          <div className='w-full'>
+            <div className='bg-orange-200 h-4/5 rounded-3xl m-6 h-screen'>
+              {children}
+            </div>
+
           </div>
 
         </div>
-
       </div>
 
     </>

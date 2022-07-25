4c7fb636b15147005cdf6184a9eded98de06d46d
Ghl24 <102258712+Ghl24@users.noreply.github.com>
2022-07-25 12:13:59 -0500

    Act form input-label (#9)
    Co-authored-by: Steven Ruiz <steven25ruiz@gmail.com>

--- src/forms/products/form-product/index.jsx
+++ src/forms/products/form-product/index.jsx
@@ -1,105 +1,65 @@
 import React from "react";
 import { Formik } from "formik";
 import { ToggleCustom } from "../../../components/Toogle";
+import { InputCustom } from "../../../components/Inputs";
+import { useForm } from "react-hook-form";
+import "../styles.scss";
 
-// import "./styles.scss";
+
 
 const FormProduct = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      precioUnit: '',
+      description: ''
+    }
+  });
   return (
-    <Formik
-      initialValues={{
-        precioUnit: undefined
-      }}
+    <form
+      className="form-product__container"
+      onSubmit={handleSubmit((data) => console.log(data))}
     >
-      {props => (<form className="form-product__container">
-        <h1 className="h1"> PRODUCTOS </h1>
-        <div className="grid grid-cols-3 gap-5 form-product__container--forms  justify-center items-center">
-          <div className="flex justify-between mb-5">
-            {/* <label htmlFor="nombre">Nombre: </label> */}
-            <input
-              type="text"
-              id="nombre"
-              name="name"
-              placeholder="   Nombre "
-            ></input>
-          </div>
-          <div className="flex justify-between mb-5">
-            {/* <label htmlFor="descripcion">Descripcion: </label> */}
-            <input
-              type="text"
-              id="descripcion"
-              name="descripcion"
-              placeholder="   Descripcion "
-            ></input>
-          </div>
-          <div className="flex justify-between mb-5">
-            <ToggleCustom title="Estatus:" />
-
-            <ToggleCustom title="Bowl:" />
-          </div>
-          {console.log(props.values.precioUnit)}
-          <div className="flex justify-between mb-5 input-container">
-            <label htmlFor="precio unit" className={`${props.values.precioUnit ? 'input-label-active' : 'input-label'}`}>Precio unit: </label>
-            <input
-              type="number"
-              id="precio_unit"
-              name="precioUnit"
-              className="input-machine"
-              onChange={props.handleChange}
-              onBlur={props.handleBlur}
-              value={props.values.name}
-              placeholder=" "
-            ></input>
-          </div>
-          <div className="flex justify-between mb-5">
-            <label htmlFor="precio cost">Precio cost: </label>
-            <input
-              type="number"
-              id="precio cost"
-              name="precio cost"
-              placeholder=" "
-            ></input>
-          </div>
-          <div className="flex justify-between mb-5">
-            <label htmlFor="discont">Discont: </label>
-            <input
-              type="number"
-              id="discont"
-              name="discont"
-              placeholder=" "
-            ></input>
-          </div>
-          <div className="flex justify-between mb-5">
-            <label htmlFor="unidad">Unidad: </label>
-            <input
-              type="text"
-              id="unidad"
-              name="unidad"
-              placeholder="   Ej: kg, gr, lb "
-            ></input>
-          </div>
-          <div className="flex justify-between mb-5">
-            <label htmlFor="unidad minima">Unidad minima: </label>
-            <input
-              type="number"
-              id="unidad minima"
-              name="unidad minima"
-              placeholder=" "
-            ></input>
-          </div>
-          <div className="flex justify-between mb-5">
-            <label htmlFor="stock">En stock: </label>
-            <input
-              type="number"
-              id="stock"
-              name="stock"
-              placeholder=" "
-            ></input>
-          </div>
+      <h1 className="h1"> PRODUCTOS </h1>
+      <div className="grid grid-cols-3 gap-5 form-product__container--forms  justify-center items-center">
+        <div className="flex justify-between mb-5">
+        <InputCustom type="text" title="Nombre" watch={watch} name="name" register={register} />
         </div>
-      </form>)}
+        <div className="flex justify-between mb-5">
+          <InputCustom type="text" title="Descripcion" watch={watch} name="description" register={register} />
+        </div>
+        <div className="flex justify-between mb-5">
+          <ToggleCustom title="Estatus:" />
 
-    </Formik>
+          <ToggleCustom title="Bowl:" />
+        </div>
+        <div className="flex justify-between mb-5 input-container">
+        <InputCustom type="number" title="Precio unit" watch={watch} name="precio-unit" register={register} />
+        </div>
+        <div className="flex justify-between mb-5">
+        <InputCustom type="number" title="Precio cost" watch={watch} name="precio-cost" register={register} />
+        </div>
+        <div className="flex justify-between mb-5">
+        <InputCustom type="number" title="Discont" watch={watch} name="discont" register={register} />
+        </div>
+        <div className="flex justify-between mb-5">
+        <InputCustom type="text" title="Unidad" watch={watch} name="unidad" register={register} />
+
+        </div>
+        <div className="flex justify-between mb-5">
+        <InputCustom type="number" title="Unid min" watch={watch} name="unid-min" register={register} />
+
+        </div>
+        <div className="flex justify-between mb-5">
+        <InputCustom type="number" title="En stock" watch={watch} name="en-stock" register={register} />
+
+        </div>
+      </div>
+    </form>
   );
 };
 

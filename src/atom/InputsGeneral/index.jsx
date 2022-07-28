9f56cc4c070864b73579f298cb1f09b02ba85f06
Ghl24 <102258712+Ghl24@users.noreply.github.com>
2022-07-28 10:41:05 -0500

    styles and inputs (#19)
    * Act form input-label
    
    * prueba de commit
    
    * Act changes
    
    * Act forms and folder
    
    * create input and styles
    
    * delete watch
    
    Co-authored-by: Steven Ruiz <steven25ruiz@gmail.com>

---
+++ src/atom/InputsGeneral/index.jsx
@@ -0,0 +1,16 @@
+import './style.scss';
+  
+  
+  export const InputG = (props) => {
+    const{title,type,register,name}= props;
+  
+    return (
+      <div className="AppStyle" >
+        <form className="formStyle">
+          <label className="label-style">{title}</label>
+          <input className="inputStyle" {...register(name)} type={type} />
+        </form>
+      </div>
+    );
+  }
+  
